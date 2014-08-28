b603fc26b6aea08805bd8f893d33ee50ad7e5a84
carlosmontes002 <carlosmontes002@gmail.com>
2014-08-28 10:28:27 -0430

    add Pie, Donut and Gauge charts

--- directive/ngC3.js
+++ directive/ngC3.js
@@ -4,17 +4,19 @@ angular.module("ngC3", [])
     .directive("ngC3", function () {
 
         function isEmptyJSON(obj) {
-            for(var i in obj) { return false; }
+            for (var i in obj) {
+                return false;
+            }
             return true;
         }
 
-        function seriesTransform (series) {
-            var xs      = [];
-            var types   = {};
+        function getData(series) {
+            var xs = [];
+            var types = {};
             var columns = [];
-            var _x      = [];
-            var _y      = [];
-            var yAxis   = {};
+            var _x = [];
+            var _y = [];
+            var yAxis = {};
 
             series.forEach(function (serie) {
                 if (serie.yAxis) {
@@ -52,6 +54,17 @@ angular.module("ngC3", [])
             return data;
         }
 
+        function getPiesData(series) {
+            var columns = [];
+
+            series.forEach(function (serie) {
+                var data = _.clone(serie.data);
+                data.unshift(serie.name);
+                columns.push(data);
+            });
+            return columns;
+        }
+
         return {
             restrict: "AE",
             template: "<div></div>",
@@ -65,30 +78,34 @@ angular.module("ngC3", [])
                 var chartElement = element[0].childNodes[0];
                 chartElement.id = scope.chartId;
 
-                scope.$watch("[series, options]", function (newChanges){
+                scope.$watch("[series, options]", function (newChanges) {
                     newChanges[1] = newChanges[1] ? newChanges[1] : {};
-                    var body  = {};
+                    var transform = newChanges[1].transform ? newChanges[1].transform : null;
+                    var body = {};
                     var typeChart = newChanges[1].type ? newChanges[1].type : "other";
 
-                    switch(typeChart) {
+                    switch (typeChart) {
                         case "donut":
                         case "pie":
                         case "gauge":
-
+                            body["data"] = {
+                                type: typeChart,
+                                columns: getPiesData(newChanges[0])
+                            };
                         break;
                         default:
-                            body["data"] = seriesTransform(newChanges[0]);
+                            body["data"] = getData(newChanges[0]);
 
                             if (newChanges[1]) {
                                 _.merge(body, newChanges[1]);
                                 body.data["groups"] = newChanges[1].groups ? newChanges[1].groups : [];
                             }
-
-                            body["bindto"] = scope.chartId ? "#" + scope.chartId : "#chart";
                         break;
                     }
-
+                    body["bindto"] = scope.chartId ? "#" + scope.chartId : "#chart";
                     var chart = c3.generate(body);
+
+                    if (transform) { chart.transform(transform); }
                 }, true);
             }
         }
